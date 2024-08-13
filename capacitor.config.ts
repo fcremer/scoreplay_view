85ca67539f42346d8db1acea40baef414c0978d6
fcremer <fabian.cremer@amadeus.com>
2024-08-13 09:06:26 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'scoreplay_view',
+  webDir: 'www'
+};
+
+export default config;
